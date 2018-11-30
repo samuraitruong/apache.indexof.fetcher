b78027796da7f67fe9cbb0882497ac4cc7698c9d
Truong Nguyen <samuraitruong@hotmail.com>
2018-11-30 22:22:28 +1100

    initial

--- IndexOf.Fetcher/MainWindow.xaml.cs
+++ IndexOf.Fetcher/MainWindow.xaml.cs
@@ -1,28 +1,43 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace IndexOf.Fetcher
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnFetch_Click(object sender, RoutedEventArgs e)
+        {
+            
+            txtResults.Document.Blocks.Clear();
+            List<string> links = new List<string>();
+            using (var client = new WebClient())
+            {
+                var html = client.DownloadString(txtUrl.Text.Trim());
+                var matches = Regex.Matches(html, "href=\\\"([^\"]*)");
+                foreach (Match match in matches)
+                {
+                    var url = match.Groups[1].Value;
+                    if (url.Contains("..")) continue;
+                    links.Add(txtUrl.Text.Trim() + match.Groups[1].Value);
+                }
+                var text = String.Join("\r\n", links);
+                Clipboard.SetText(text);
+
+                txtResults.AppendText(text);
+                lblStatus.Content = $"Copied {links.Count} links to clipboard";
+            }
         }
     }
 }
